281b588b3313d4af19ae6a8c4d2a8423de15b632
Nathan Yu <nathan5508@gmail.com>
2023-10-22 00:04:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Document Assistant
+An app that enabled user to do fuzzy search on the all the documents in a directory and all it's sub folders
+
+Built with python and streamlit
+
+### Instructions
+- install the dependencies `pip3 install -r requirements.txt`
+- set the top level directory path with `export TOP_DIR=/Path/to/my/documents`
+- run the app `python3 -m streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,102 @@
+from docx import Document
+import os
+from thefuzz import fuzz
+import streamlit as st
+import time
+import subprocess
+
+SUPPORTED_FILES = {".docx"}
+
+
+class Paragraph:
+    score = 0
+
+    def __init__(self, text, file_path) -> None:
+        self.text = text
+        self.file_path = file_path
+
+    def __repr__(self) -> str:
+        return f"""\nFile path: {self.file_path}\nText: {self.text}\n"""
+
+
+class Indexer:
+
+    paragraphs = []
+
+    def __init__(self, top_dir) -> None:
+        self.top_dir = top_dir
+
+    def index(self):
+        print(f"Start indexing directory {self.top_dir}...")
+        start = time.time()
+        for (root, _, files) in os.walk(self.top_dir, topdown=True):
+            for file in files:
+                if os.path.splitext(file)[1] == ".docx" and not file.startswith("~$"):
+                    full_path = os.path.join(root, file)
+                    doc = Document(full_path)
+                    for p in doc.paragraphs:
+                        words = p.text.split()
+                        num_commas = p.text.count(",")
+                        if not num_commas:
+                            num_commas = 1
+                        # only read paragraphs with more than five words and have a word/comma ratio of 2
+                        if len(words) > 5 and len(words)/num_commas > 3:
+                            self.paragraphs.append(
+                                Paragraph(p.text, full_path))
+                # todo: add pdf support
+                # if os.path.splitext(file)[1] == ".pdf":
+                #     full_path = os.path.join(root,file)
+                #     doc = Document(full_path)
+                #     for p in doc.paragraphs:
+                #         if len(p.text.split())>5:
+                #             self.paragraphs.append(Paragraph(p.text, full_path))
+        print(f"Completed indexing in {time.time() - start} seconds!")
+
+    def search(self, query, num_results=3):
+        ratios = []
+        results = []
+        for p in self.paragraphs:
+            score = fuzz.token_set_ratio(p.text, query)
+            ratios.append(score)
+            p.score = score
+        sorted_indexes = sorted(
+            range(len(ratios)), key=lambda k: ratios[k], reverse=True)
+
+        for i in sorted_indexes[:num_results]:
+            results.append(self.paragraphs[i])
+        return results
+
+
+@st.cache_resource(show_spinner=False)
+def init_indexer(dir_path):
+    indexer = Indexer(dir_path)
+    indexer.index()
+    return indexer
+
+
+if __name__ == "__main__":
+    directory = os.environ.get("TOP_DIR")
+    if not directory:
+        raise RuntimeError("Environment variable `TOP_DIR` must be set!")
+    with st.spinner(f'Indexing directory {directory}...'):
+        start = time.time()
+        indexer = init_indexer(directory)
+    st.success(f'Completed Indexing in {int(time.time() - start)} seconds')
+    st.title("Document Search")
+    search_input = st.text_input("Enter keywords...")
+    if search_input:
+        results = indexer.search(search_input, num_results=10)
+        for i, paragraph in enumerate(results):
+            text = f"""
+            **File**: {paragraph.file_path}
+
+            **Line**: {paragraph.text}
+
+            **Match Score**: {paragraph.score}
+            """
+            st.markdown(text)
+
+            if st.button("Open File", type="primary", key=i):
+                subprocess.check_output(
+                    f"open '{paragraph.file_path}'", shell=True)
+            st.divider()
